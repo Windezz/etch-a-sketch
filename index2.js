82ee96ea6d35db77bc42732e43c43e70b2e95b19
Windezz <123edwinthung@gmail.com>
2022-05-24 09:16:26 +0800

    finished the main feature

--- index2.js
+++ index2.js
@@ -1,33 +1,59 @@
 const container = document.querySelector('#container');
-const button = document.querySelector('button');
+const button = document.querySelector('#grid');
+const rainbow = document.querySelector('#rainbow');
+let column = 16;
+let rown = column-1;
 
-
-function makeRows(rown) {
-    for (let i = 0; i < rown; i++) {
-        let row = document.createElement('div');
-
-        // change div's bg color when mouse hover
-        row.onmouseover = function() {
-            this.style.backgroundColor = 'black';
+// add an eventlistener to the button 
+button.addEventListener('click', () => {
+    column = prompt('Enter the grid number (< 100): ');
+    if (column > 100) {
+        alert('invalid grid number, grid number should be less than 100');
+    } else {
+        rown = column-1;
+        console.log(column, rown);
+    
+        // remove the child elements in the div
+        var child = container.lastElementChild;
+        while (child) {
+            container.removeChild(child);
+            child = container.lastElementChild;
         }
-
-        container.appendChild(row).className = 'gridRow';
+    
+        // insert the new grid into the div
+        makeColumns(column);
+        makeRows(rown);
     }
-}
+})
+
 
 function makeColumns(column) {
+    let size = 640;
+    let width = size/column;
+    let height = size/column;
     for (let i = 0; i < column; i++) {
-        makeRows(16);
+        let column = document.createElement('div');
+
+        // change div's bg color when mouse hover
+        column.onmouseover = function() {
+            this.style.backgroundColor = 'black';
+        }
+        column.setAttribute('style', `background-color: white; width: ${width}px; height: ${height}px`);
+        container.appendChild(column).className = 'gridCol';
     }
 }
 
-function setRows() {
-    const rowSelect = document.querySelectorAll('.gridRow')
-    rowSelect.forEach((row) => {
-        row.setAttribute('style', 'background-color: white; width: 40px; height: 40px');
-    })
+function makeRows(rown) {
+    if (rown === 0) {
+        return;
+    } else {
+        for (let i = 0; i < rown; i++) {
+            makeColumns(column);
+            console.log('create row');
+        }
+    }
 }
 
-makeRows(16);
-makeColumns(15);
-setRows();
+makeColumns(column);
+makeRows(rown);
+
